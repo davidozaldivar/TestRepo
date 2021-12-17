598810dcddbaef8f151c0502b501dda757ad72d2
David <David58117@yahoo.com>
2021-12-16 20:01:02 -0600

    AddedPrintNumbs To 10

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,15 +1,23 @@
-public class MainClass
-{
+public class MainClass {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hi! This is main");
 
         TestTwo();
 
+        PrintNum();
+
     }
 
-   public static void TestTwo(){
-       System.out.println("TestTwo!");
-   }
+    public static void TestTwo() {
+        System.out.println("TestTwo!");
+    }
+
+    public static void PrintNum() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " ");
+        }
+
+    }
 }
